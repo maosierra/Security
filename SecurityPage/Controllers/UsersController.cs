8b217d48ff7647eac21aa10454b9f73f3817c944
maosierra <fidodido18@hotmail.com>
2022-11-20 10:30:12 -0500

    Users page

---
+++ SecurityPage/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecurityPage.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecurityPage.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var users = context.Users.ToList();
+            ViewBag.Users = users;
+            return View();
+        }
+    }
+}
+
