73fb96267b1ca07c7a2280ee9e4eb193146ba578
maosierra <fidodido18@hotmail.com>
2022-11-20 10:29:45 -0500

    Add users page

---
+++ SecurityPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SecurityPage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
